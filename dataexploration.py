a9e6fa9431e5550c2a3abfbe27889e433cb4fbd0
AntonioMaddaloni <a.maddaloni10@studenti.unisa.it>
2022-02-03 17:10:37 +0100

    data exploration

---
+++ dataexploration.py
@@ -0,0 +1,28 @@
+# To support both python 2 and python 3
+from __future__ import division, print_function, unicode_literals
+
+# Common imports
+#as rename 
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#load our data
+datapath = os.path.join("dataset", "")
+
+covid = pd.read_csv(datapath + "covid_data_2020-2021.csv")
+
+#distribution of data
+print(covid.describe())
+
+print("\n\nanalisi test date")
+print(covid["test_date"].describe())
+
+print("\n\nanalisi gender")
+print(covid["gender"].describe())
+
+print("\n\nanalisi test_indication")
+print(covid["test_indication"].describe())
+
+print(covid.isnull().sum())
